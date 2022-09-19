43280c6928522baecbd2d123284ecff8f881ede8
MikemanCockrell <mcockrell@email.arizona.edu>
2022-09-18 17:21:00 -0700

    Camera fix plus thruster balancing

--- HW2_ScottsTots_GAME351/Assets/Scripts/SmoothCam.cs
+++ HW2_ScottsTots_GAME351/Assets/Scripts/SmoothCam.cs
@@ -7,6 +7,7 @@ public class SmoothCam : MonoBehaviour
     //assigns a public tranform which is the point on the car that the camera follows. It is a list of the three different transforms of the different cars.
     //offset is a vector3 that effect where the camera is relative to the follow point. I have found that only giving a y offset works the best.
     //x and z offsets cause the car to come off center so i put the follow point attach to the car and behind it.
+    public Transform[] followPoints = new Transform[3];
     public Transform[] crafts = new Transform[3];
     public Vector3 offset;
     public int selected = 0;
@@ -31,10 +32,9 @@ public class SmoothCam : MonoBehaviour
     void FixedUpdate()
     {
         //every frame it sets the cameras position to the follow points position plus an offest.
-        this.transform.position = crafts[selected].position + offset;
+        this.transform.position = followPoints[selected].position + offset;
         //It copys the y orientation of the craft. I had to use vector3 to make it work so it copies its own x and z orientation to that it stays level.
-        Vector3 newRotation = new Vector3(this.transform.eulerAngles.x, crafts[selected].transform.eulerAngles.y, this.transform.eulerAngles.z);
-        this.transform.eulerAngles = newRotation;
-       
+        this.transform.LookAt(crafts[selected].position);
+
     }
 }
